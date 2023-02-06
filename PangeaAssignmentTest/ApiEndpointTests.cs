01ec7c5752421e912817566ad3def5bff7a1c800
g.turdziladze@gmail.com <g.turdziladze@gmail.com>
2023-02-06 01:15:49 +0100

    removed old test files

--- PangeaAssignmentTest/ApiEndpointTests.cs
+++
@@ -1,7 +0,0 @@
-﻿[TestClass]
-Public Class ApiEndpointTests
-
-        protected static string URL = "https://localhost:44372/api/"; //TestApi URL should go here 
-        protected static HttpClient Client = new HttpClient();
-
-End Class
